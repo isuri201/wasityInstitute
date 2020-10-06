3efc72da8da41baa1a6e5893f868ef90926c02f7
LalithK90 <asakahatapitiya@gmail.com>
2020-10-06 12:45:30 +0530

    Role module back end part completed

--- src/main/java/lk/wasityInstitute/asset/userManagement/dao/RoleDao.java
+++ src/main/java/lk/wasityInstitute/asset/userManagement/dao/RoleDao.java
@@ -1,10 +1,11 @@
 package lk.wasityInstitute.asset.userManagement.dao;
 
+
 import lk.wasityInstitute.asset.userManagement.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleDao extends JpaRepository<Role, Integer > {
+public interface RoleDao extends JpaRepository< Role, Integer > {
     Role findByRoleName(String roleName);
 }
